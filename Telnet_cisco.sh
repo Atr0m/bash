3de6d736ae1edf545b1780ba8922f4c0bc7dbaea
Jorge Campo <atr0mico@gmail.com>
2014-03-05 14:34:47 +0100

    Create Telnet_cisco.sh

---
+++ Telnet_cisco.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+host=192.168.1.123
+port=23
+user=myuser
+pass=mypassword
+cmd1="show run"
+
+( echo open ${host} ${port}
+sleep 3
+echo ${user}
+sleep 1
+echo ${pass}
+sleep 1
+echo ${cmd1}
+sleep 1
+echo exit ) | telnet
